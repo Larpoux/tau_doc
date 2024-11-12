ac5b972819e4bd7fb3713f88fbd0db89cdcc05ba
larpoux <larpoux@gmail.com>
2024-11-12 17:01:47 +0100

    doc

--- _site/live_etau_example/flutter_bootstrap.js
+++ _site/live_etau_example/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2397695492"
+    serviceWorkerVersion: "2593229459"
   }
 });
