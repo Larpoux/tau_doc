96b7170f879885048e913b7a25b2915bf0502c40
larpoux <larpoux@gmail.com>
2025-02-10 14:41:50 +0100

    doc

--- bin/pub.sh
+++ bin/pub.sh
@@ -5,6 +5,14 @@
 #gsed -i "s/^FS_VERSION:.*/FS_VERSION: $FLUTTER_SOUND_VERSION/" _config.yml
 rm -rf _site
 cp ../flutter_sound/README.md pages/fs
+rm -rf live/fs
+cp -r ../flutter_sound/example/build/web live/fs
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit -1
+fi
+
+
 
 echo 'config set --local path ~/vendor/bundle'
 bundle config set --local path '~/vendor/bundle'
